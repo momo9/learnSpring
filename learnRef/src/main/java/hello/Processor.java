8cdeb9fd95e3e72a85cd98c2bc33a07c15e2e10f
momo9 <liangchenchi@gmail.com>
2015-02-06 11:50:46 +0800

    [spring] setter-based dependency injection

--- learnRef/src/main/java/hello/Processor.java
+++ learnRef/src/main/java/hello/Processor.java
@@ -12,6 +12,9 @@ public class Processor {
     private MessageService service;
     private String author;
 
+    private MessageService errService;
+    private int number;
+
     @ConstructorProperties({"service", "author"})
     public Processor(MessageService service, String author) {
         this.service = service;
@@ -21,5 +24,14 @@ public class Processor {
     public void run() {
         Logger logger = Logger.getLogger(Processor.class);
         logger.info(String.format("%s: %s", author, service.getMessage()));
+        logger.error(String.format("%s: %d", errService.getMessage(), number));
+    }
+
+    public void setErrService(MessageService errService) {
+        this.errService = errService;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
     }
 }
