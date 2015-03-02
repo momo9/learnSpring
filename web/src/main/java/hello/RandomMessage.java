1e3a1f66e0da8c2583fcb097be46f00e6ef5d204
momo9 <liangchenchi@gmail.com>
2015-03-02 11:49:40 +0800

    component scan & autowired

--- web/src/main/java/hello/RandomMessage.java
+++ web/src/main/java/hello/RandomMessage.java
@@ -1,12 +1,13 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RandomMessage implements MessageService {
 
+    @Autowired
     private Message message;
-
-    public void setMessage(Message message) {
-        this.message = message;
-    }
 
     public Message message() {
         return message;
