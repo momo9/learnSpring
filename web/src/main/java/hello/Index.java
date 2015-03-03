70bc05d462185b2d1bcc4b06a9728b2f497e4a26
momo9 <liangchenchi@gmail.com>
2015-03-03 11:45:08 +0800

    simple mvc

--- web/src/main/java/hello/Index.java
+++ web/src/main/java/hello/Index.java
@@ -1,38 +1,17 @@
 package hello;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.naming.Context;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.log4j.Logger;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
-public class Index extends javax.servlet.http.HttpServlet {
-
-    private ApplicationContext context;
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        Logger logger = Logger.getLogger(Index.class);
-        logger.debug("do get");
-        
-        PrintWriter writer = resp.getWriter();
-        context = WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext());
-        MessageService service = context
-                .getBean("randomMessage", MessageService.class);
-        writer.print(service.message());
-
-        ObjOne objOne = context.getBean("objOne", ObjOne.class);
-        ObjTwo objTwo = context.getBean("objTwo", ObjTwo.class);
-        
-        objOne.setStr("one");
-        objTwo.setStr("two");
-        
+@Controller
+public class Index {
+    
+    @RequestMapping("/new/{val}")
+    public String doGet(@PathVariable String val, Model model) {
+        model.addAttribute("message", val);
+        return "/show";
     }
+    
 }
